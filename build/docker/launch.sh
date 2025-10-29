246ad53f3118dbc64a866cbda2b0849a3627debe
croth <christopher.roth@noris.de>
2025-10-29 10:17:07 +0000

    resolve mounting-problems

--- build/docker/launch.sh
+++ build/docker/launch.sh
@@ -65,6 +65,34 @@ if [[ ! -f /data/mods/simplebackups.jar ]]; then
     https://mediafilez.forgecdn.net/files/6911/698/SimpleBackups-1.21-4.0.19.jar
 fi
 
+SP="/data/server.properties"
+DEFAULT_SP="/opt/defaults/server.properties"
+
+# 1) Vorlage einmalig auf PVC kopieren (falls noch nicht vorhanden)
+if [[ ! -f "$SP" ]]; then
+  if [[ -f "$DEFAULT_SP" ]]; then
+    cp -f "$DEFAULT_SP" "$SP"
+  else
+    printf "allow-flight=true\nwhite-list=true\nmax-tick-time=180000\nmotd=All the Mods 10\n" > "$SP"
+  fi
+fi
+
+# 2) Schreibrechte für Gruppe geben (fsGroup hat Group-Ownership)
+chmod g+rw "$SP" || true
+# optional: gesamten Ordner gruppenschreibbar machen
+chmod -R g+rwX /data || true
+
+# 3) MOTD setzen – ohne -i (aber auf die PVC-Datei, nicht die CM)
+if [[ -n "${MOTD:-}" ]]; then
+  tmp="$(mktemp)"
+  awk -v m="$MOTD" '
+    BEGIN{set=0}
+    /^motd=/{print "motd=" m; set=1; next}
+    {print}
+    END{if(!set) print "motd=" m}
+  ' "$SP" > "$tmp" && mv "$tmp" "$SP"
+fi
+
 [[ -n "${MOTD:-}" ]] && sed -i "s/^motd=.*/motd=${MOTD}/" server.properties
 
 [[ -n "${OPS:-}" ]] && echo "$OPS" | tr ',' '\n' > ops.txt
